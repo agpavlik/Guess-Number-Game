a7f8ea0387d884f60faa7860318ad4d8a1753761
agpavlik <agpavlik@gmail.com>
2024-04-13 22:21:30 -0400

    Add page structure and button

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Boo!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <StartGameScreen />;
 }
 
 const styles = StyleSheet.create({
