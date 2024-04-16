68c79069657357bf514d126ab304b603e7328ae2
agpavlik <agpavlik@gmail.com>
2024-04-15 20:11:48 -0400

    Upgrade background style

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { StyleSheet, Text, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#ddb59f",
   },
 });
