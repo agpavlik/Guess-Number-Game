605ff23503e2a9318e7b98af4ef641afec2744cc
agpavlik <agpavlik@gmail.com>
2024-04-21 20:32:17 -0400

    Fix an error

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 
 import Colors from "../../constants/colors";
 
@@ -12,12 +12,20 @@ function NumberContainer({ children }) {
 
 export default NumberContainer;
 
+// Get the device width by using dimensions.
+// Get method which takes an argument, which is a string,
+// and holds either a value of screen or window.
+// (Window is excluding the status bar)
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
   container: {
     // borderWidth: 4,
     // borderColor: Colors.accent900,
-    padding: 24,
-    margin: 24,
+    // padding: 24,
+    // margin: 24,
+    padding: deviceWidth < 380 ? 24 : 36,
+    margin: deviceWidth < 380 ? 12 : 24,
     borderRadius: 8,
     elevation: 8,
     alignItems: "center",
@@ -29,7 +37,8 @@ const styles = StyleSheet.create({
   },
   numberText: {
     color: Colors.accent500,
-    fontSize: 36,
+    // fontSize: 36,
+    fontSize: deviceWidth < 380 ? 28 : 36,
     // fontWeight: "bold",
     fontFamily: "open-sans-bold",
   },
